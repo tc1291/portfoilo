ba853c1cfd740778f9a67379249070c5163bb5ec
tc1291 <timmychen1129@gmail.com>
2026-04-27 14:20:38 +0800

    第一版

---
+++ 411171616/assets/js/script.js
@@ -0,0 +1,15 @@
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
